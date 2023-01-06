ba124a36d3fe2916c5adbab36b028e36b075884c
Tjalf Stadel <tjalf.stadel@web.de>
2023-01-06 22:50:23 +0100

    Implemented own cache class

--- src/mace/domain/cache.py
+++ src/mace/domain/cache.py
@@ -2,27 +2,13 @@ from mace.domain.vector import Vector
 
 
 class Cache_segments:
-    def __init__(self) -> None:
+    def __init__(self, fun) -> None:
+        self.fun = fun
         self.dictionary = dict()
 
-    def __call__(self, *args: tuple, **kwds: tuple) -> int:
-        input = (args, kwds)
-        if input in self.dictionary:
-            return self.dictionary[input]
-
-
-@Cache_segments
-def add_vector(a: Vector, b: Vector, name: str):
-    return a + b
-
-
-def main():
-    one = Vector(1, 1, 1)
-    two = Vector(0, 0, 0)
-
-    for _ in range(100):
-        two = add_vector(one, two, "test")
-
-
-if __name__ == "__main__":
-    main()
+    def __call__(self, *args) -> None:
+        if args in self.dictionary:
+            return self.dictionary[args]
+        output = self.fun(*args)
+        self.dictionary[args] = output
+        return output
