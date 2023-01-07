4f136db4fd26315881d51f643d6fa88caaa0ab79
Tjalf Stadel <tjalf.stadel@web.de>
2023-01-07 18:31:16 +0100

    added test file

---
+++ src/mace/test/unittest.py
@@ -0,0 +1,17 @@
+import unittest
+from mace.domain.vector import Vector
+
+class TestVector(unittest.TestCase):
+    def setUp(self) -> None:
+        self.a = Vector(0,1,3)
+        self.b = Vector(2,4,7)
+
+    def test_add(self):
+        sum = self.a + self.b
+        self.assertEqual(sum, Vector(2,5,10))
+
+    def test_true(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
