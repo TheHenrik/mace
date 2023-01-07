8678b0d9ebc356cd24b4e7cec1da49eeb6a223dd
Tjalf Stadel <tjalf.stadel@web.de>
2023-01-07 16:36:30 +0100

    Changed __init__.py to allow direct import

--- src/mace/__init__.py
+++ src/mace/__init__.py
@@ -1,5 +1,11 @@
+from mace.domain.plane import FlugzeugParser, Rumpf
+from mace.domain.vector import Vector, Vectorcalc
 from mace.gui.gui import hello_world
+from mace.mass.calc import get_mass_aircraft
+
+import mace.domain
 
 
 def gui():
+    """Entry point for 'mace-gui'"""
     hello_world()
