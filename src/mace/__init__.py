559276a1945af8684fcffbcea59f7ebe06fab37d
Tjalf Stadel <tjalf.stadel@web.de>
2023-01-07 17:57:52 +0100

    Changed gui entry point

--- src/mace/__init__.py
+++ src/mace/__init__.py
@@ -1,10 +1,4 @@
-import mace.domain
 from mace.domain.plane import FlugzeugParser, Rumpf
 from mace.domain.vector import Vector, Vectorcalc
 from mace.gui.gui import hello_world
 from mace.mass.calc import get_mass_aircraft
-
-
-def gui():
-    """Entry point for 'mace-gui'"""
-    hello_world()
