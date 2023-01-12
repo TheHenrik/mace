421061e725f7b8cac56a3f3b768d8303648ccef8
Tjalf Stadel <tjalf.stadel@web.de>
2023-01-12 11:01:44 +0100

    changed tests

--- bin/swinger.py
+++ bin/swinger.py
@@ -5,10 +5,10 @@ from mace.test import performance_time, performance_report
 def main():
     flugzeug = FlugzeugParser("flugzeug.xml").build_plane()
     flugzeug.weight = get_mass_aircraft(flugzeug)
-    #print(f"The weight of the plane is approx. {flugzeug.weight} kg!")
+    return flugzeug.weight
 
 
 if __name__ == "__main__":
-    main()
-    #print(f'{performance_time(1_000, main)*1e3:.3f}ms')
-    #performance_report(performance_time,1_000, main)
+    print(f"The weight of the plane is approx. {main()} kg!")
+    performance_time(10_000, main)
+    performance_report(performance_time,1_000, main, output = None)
