34d792cc1d57a0ac0acaac8fe7a3f0e3aa3aac76
Tjalf Stadel <tjalf.stadel@web.de>
2023-01-12 19:23:12 +0100

    trying to fix vol bug

--- src/mace/mass/calc.py
+++ src/mace/mass/calc.py
@@ -17,7 +17,7 @@ def tri_volume(first, second, third):
 def gen_profile(profil, start_innen, end_innen, start_außen, end_außen):
     innen_strecke = end_innen - start_innen
     außen_strecke = end_außen - start_außen
-    innen_außen = start_außen - start_innen
+    innen_außen = (start_außen - start_innen)/np.linalg.norm(start_außen - start_innen)
     höhen_strecke = np.cross(innen_außen, innen_strecke)
 
     def scale(factors, vecs):
