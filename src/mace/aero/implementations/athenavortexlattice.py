f7d14af4fd863887c402dc0fa160eb2587ae1117
GZ4L <123930326+GZ4L@users.noreply.github.com>
2023-02-26 18:08:08 +0100

    optimizations in reading avl output files

--- src/mace/aero/implementations/athenavortexlattice.py
+++ src/mace/aero/implementations/athenavortexlattice.py
@@ -1,10 +1,13 @@
-import os                       # operation system
+import os  # operation system
 import numpy as np
 from mace.aero.implementations import runsubprocess as runsub
 
 
-def avl_output(avl_file, mass_file, run_file, total_forces_file_name, strip_forces_file_name, maschine_readable_file=True):
-
+def avl_output(avl_file, mass_file, run_file, total_forces_file_name, strip_forces_file_name,
+               run_case: int = 1, maschine_readable_file=True):
+    """
+    For run_case != 1 pleas check runcase is available!
+    """
     # --- Input file writer---
 
     if os.path.exists("total_forces_avl"):
@@ -17,12 +20,14 @@ def avl_output(avl_file, mass_file, run_file, total_forces_file_name, strip_forc
     input_file.write("MASS {0}\n".format(mass_file))
     input_file.write("CASE {0}\n".format(run_file))
     input_file.write("OPER\n")
-    input_file.write("XX\n")                            # execute all runcases
+    if run_case != 1:                                           # select run_case
+        input_file.write("{0}\n".format(run_case))
+    input_file.write("X\n")                         # execute runcase, XX executes all runcases but uses last runcase
     if maschine_readable_file:
-        input_file.write("MRF\n")                       # maschine readable file
-    input_file.write("FT\n")                            # write total forces
+        input_file.write("MRF\n")                               # maschine readable file
+    input_file.write("FT\n")                                    # write total forces
     input_file.write("{0}\n".format(total_forces_file_name))
-    input_file.write("FS\n")                            # write strip forces
+    input_file.write("FS\n")                                    # write strip forces
     input_file.write("{0}\n".format(strip_forces_file_name))
 
     input_file.write("\n")
@@ -32,34 +37,39 @@ def avl_output(avl_file, mass_file, run_file, total_forces_file_name, strip_forc
 
     # ---Run AVL---
 
-    cmd = "C:/Users/Gregor/Documents/Modellflug/Software/AVL/avl.exe < input_file_avl.in"   # external command to run
+    cmd = "C:/Users/Gregor/Documents/Modellflug/Software/AVL/avl.exe < input_file_avl.in"  # external command to run
     runsub.run_subprocess(cmd)
 
-    # total_forces = np.loadtxt("forces_avl", skiprows=12)
-    # total_forces = np.fromfile("total_forces_avl", sep=" ")
+
 def read_avl_files():
-    # string.split("/"," ")   # Bsp string splitten
-    # data.strip() # String wird von Weißraumzeichen bereinigt, geht auch nur rechts oder links (lstrip, rstrip)
     file = open("total_forces_avl")
     lines = file.readlines()
-    # print(file.read())
-    # print(file.readline())
-    # print(file.readlines())  # gibt Liste zurück
+
+# ------total_forces_avl file------
+
+    # ---Trefftz Plane---
+
+    clff: float = 0
+    cdff: float = 0
+    cyff: float = 0
+    oswaldfactor: float = 0
+
     for line in lines:
         if line.endswith("| Trefftz Plane: CLff, CDff, CYff, e\n"):
-            print(line)
             string = line.split("|")
-            # string.pop(1)
-            # print(string)
             value_string = string[0]
             values = value_string.split()
-            print(values)
-            CLff = float(values[0])
-            CDff = float(values[1])
-            CYff = float(values[2])
+            clff = float(values[0])
+            cdff = float(values[1])
+            cyff = float(values[2])
             oswaldfactor = float(values[3])
-            print("CLff = {:.4f}\n".format(CLff), "CDff = {:.4f}\n".format(CDff),
-                  "CYff = {:.4f}\n".format(CYff), "Oswaldfactor = {:.4f}\n".format(oswaldfactor))
+
+    # ---Reference Data---
+
+    s_ref: float = 0
+    c_ref: float = 0
+    b_ref: float = 0
+
     for line in lines:
         if line.endswith("| Sref, Cref, Bref\n"):
             string = line.split("|")
@@ -68,8 +78,9 @@ def read_avl_files():
             s_ref = float(values[0])
             c_ref = float(values[1])
             b_ref = float(values[2])
-            print("Reference Wing area = {:.4f}\n".format(s_ref), "Reference chord depth = {:.4f}\n".format(c_ref),
-                  "Reference Wingspan = {:.4f}\n".format(b_ref))
+
+    # ---Number of Strips---
+
     number_of_strips: int = 0
     for line in lines:
         if line.endswith("| # strips\n"):
@@ -77,29 +88,92 @@ def read_avl_files():
             value_string = string[0]
             values = value_string.split()
             number_of_strips = int(values[0])
-            print("number of strips = {0}\n".format(number_of_strips))
+
+    # ---Number of Surfaces---
+
+    number_of_surfaces: int = 0
+
+    for line in lines:
+        if line.endswith("| # surfaces\n"):
+            string = line.split("|")
+            value_string = string[0]
+            values = value_string.split()
+            number_of_surfaces = int(values[0])
+
+    # ---Number of Vortices---
+    number_of_vortices: int = 0
+
+    for line in lines:
+        if line.endswith("| # vortices\n"):
+            string = line.split("|")
+            value_string = string[0]
+            values = value_string.split()
+            number_of_vortices = int(values[0])
+
+    # ---Close file and test---
+
     file.close()
 
+    print("CLff = {:.4f}\n".format(clff))
+    print("CDff = {:.4f}\n".format(cdff))
+    print("CYff = {:.4f}\n".format(cyff))
+    print("Oswaldfactor = {:.4f}\n".format(oswaldfactor))
+
+    print("Reference Wing area = {:.4f}\n".format(s_ref))
+    print("Reference chord depth = {:.4f}\n".format(c_ref))
+    print("Reference Wingspan = {:.4f}\n".format(b_ref))
+
+    print("number of strips = {0}\n".format(number_of_strips))
+    print("number of surfaces = {0}\n".format(number_of_surfaces))
+    print("number of vortices = {0}\n".format(number_of_vortices))
+
+# ------strip_forces_avl file------
+
     file = open("strip_forces_avl")
+
     lines = file.readlines()
+    values: list = []
     for strip_number in range(number_of_strips):
-        print(strip_number+1)
         for line in lines:
-            if strip_number+1 < 10:
-                if line.startswith("   {0}".format(strip_number+1)):
-                    print(line)
-            elif strip_number+1 < 100:
-                if line.startswith("  {0}".format(strip_number+1)):
-                    print(line)
-            elif strip_number+1 < 1000:
-                if line.startswith(" {0}".format(strip_number+1)):
-                    print(line)
-            elif strip_number+1 < 10000:
-                if line.startswith("{0}".format(strip_number+1)):
-                    print(line)
+            if strip_number + 1 < 10:
+                if line.startswith("   {0}".format(strip_number + 1)) and "|" not in line:
+                    values = line.split()
+            elif strip_number + 1 < 100:
+                if line.startswith("  {0}".format(strip_number + 1)) and "|" not in line:
+                    values = line.split()
+            elif strip_number + 1 < 1000:
+                if line.startswith(" {0}".format(strip_number + 1)) and "|" not in line:
+                    values = line.split()
+            elif strip_number + 1 < 10000:
+                if line.startswith("{0}".format(strip_number + 1)) and "|" not in line:
+                    values = line.split()
             else:
                 print("No valid data format")
+        # print(values)
+
+        j = int(values[0])              # entspricht strip_number
+        xle = float(values[1])          # Xle
+        yle = float(values[2])          # Yle
+        zle = float(values[3])          # Zle
+        chord = float(values[4])        # Chord
+        area = float(values[5])         # Area
+        c_cl = float(values[6])         # c_cl
+        ai = float(values[7])           # ai
+        cl_norm = float(values[8])      # cl_norm
+        cl = float(values[9])           # cl
+        cd = float(values[10])          # cd
+        cdv = float(values[11])         # cdv
+        cm_c = float(values[12])        # cm_c / 4
+        cm_le = float(values[13])       # cm_LE
+        cpx = float(values[14])         # C.P.x / c
+
+        print("strip number = {0}\n".format(j), "Xle = {:.4f}\n".format(xle), "Yle = {:.4f}\n".format(yle),
+              "Zle = {:.4f}\n".format(zle), "Chord = {:.4f}\n".format(chord), "Area = {:.4f}\n".format(area),
+              "c_cl = {:.4f}\n".format(c_cl), "ai = {:.4f}\n".format(ai), "cl_norm = {:.4f}\n".format(cl_norm),
+              "cl = {:.4f}\n".format(cl), "cd = {:.4f}\n".format(cd), "cdv = {:.4f}\n".format(cdv),
+              "cm_c / 4 = {:.4f}\n".format(cm_c), "cm_LE = {:.4f}\n".format(cm_le), "C.P.x / c = {:.4f}\n".format(cpx))
     file.close()
+
 
 # ---Test---
 
@@ -112,5 +186,5 @@ if __name__ == "__main__":
     strip_forces_file_name = "strip_forces_avl"
 
     avl_output(avl_file, mass_file, run_file, total_forces_file_name, strip_forces_file_name,
-               maschine_readable_file=True)
+               run_case=1, maschine_readable_file=True)
     read_avl_files()
