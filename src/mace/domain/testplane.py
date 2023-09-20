fa91fcbc7d924f2769adc1fbbc1bacf7932133b7
Tjalf Stadel <tjalf.stadel@web.de>
2023-09-20 14:43:42 +0200

    fixed missing argument in testplane= plane.Plane()

--- src/mace/domain/testplane.py
+++ src/mace/domain/testplane.py
@@ -3,7 +3,9 @@ import numpy as np
 from mace.domain import plane
 
 # --- Airfoil ---
-a_airfoil_file = plane.AirfoilFile(filepath="C:/Users/Gregor/Documents/GitHub/mace/data/airfoils/n0012")
+a_airfoil_file = plane.AirfoilFile(
+    filepath="C:/Users/Gregor/Documents/GitHub/mace/data/airfoils/n0012"
+)
 a_Airfoil = plane.Airfoil(type=plane.AirfoilFile(a_airfoil_file.filepath))
 
 # --- Wing ---
@@ -16,20 +18,29 @@ a_inc = 0
 a_inc_outer = 0
 inner_airfoil = a_Airfoil
 outer_airfoil = a_Airfoil
-wingsegment1 = plane.WingSegment(nose_inner=nose_inner, nose_outer=nose_outer, back_inner=back_inner,
-                                 back_outer=back_outer, a_inc=a_inc, a_inc_outer=a_inc_outer,
-                                 inner_airfoil=inner_airfoil, outer_airfoil=outer_airfoil)
+wingsegment1 = plane.WingSegment(
+    nose_inner=nose_inner,
+    nose_outer=nose_outer,
+    back_inner=back_inner,
+    back_outer=back_outer,
+    a_inc=a_inc,
+    a_inc_outer=a_inc_outer,
+    inner_airfoil=inner_airfoil,
+    outer_airfoil=outer_airfoil,
+)
 
 
 segments = [wingsegment1]
 wing = plane.Wing(segments=segments)
 
 # --- Propulsion ---
-thrust: np.ndarray = np.array([[0, 20], [5, 19], [10, 18], [15, 16], [20, 14], [30, 10], [50, 5]])
+thrust: np.ndarray = np.array(
+    [[0, 20], [5, 19], [10, 18], [15, 16], [20, 14], [30, 10], [50, 5]]
+)
 # [[v0, f0], [v1, f1], [v2, f2], [v3, f3], ...]
 propulsion = plane.Propulsion(thrust=thrust)
 
 # --- Plane ---
 name = "testplane"
 mass = 5
-testplane = plane.Plane(name=name, wing=wing, mass=mass, propulsion=)
+testplane = plane.Plane(name=name, wing=wing, mass=mass, propulsion=propulsion)
