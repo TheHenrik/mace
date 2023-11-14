00a18d3d0abd5998d8295e3b1fcfd2d34461f1af
Tjalf Stadel <tjalf.stadel@web.de>
2023-11-14 20:44:23 +0100

    added docstrings for the results class

--- src/mace/utils/results.py
+++ src/mace/utils/results.py
@@ -11,10 +11,25 @@ class Results:
         self.headers = []
 
     def push(self, headers, values):
+        """Adds headers and values to itself
+
+        Args:
+            headers (any): A single name or a list of names of data
+            values (any): A single value or a list of values of data
+        """
         self.headers.extend(list(map(str, headers)))
         self.data.extend(list(map(str, values)))
 
     def as_csv_line(self, delimitter: str=",", header=False) -> str:
+        """Returns the collected data in a string using the given delimiters for use in a csv file
+
+        Args:
+            delimitter (str, optional): The chosen delimitter for the csv. Defaults to ",".
+            header (bool, optional): also returns the header if selected. Defaults to False.
+
+        Returns:
+            str: csv usable string of the collected data
+        """
         line = f" {delimitter}".join(self.data)
         if header:
             line = f" {delimitter}".join(self.data) + line
